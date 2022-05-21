1e37fef39cf961da5e6639617c6aee06067d9c04
Leon Si <contact@leonzalion.com>
2022-05-21 11:25:27 -0400

    chore: update deps

--- test/suites/get-secure-input-processes.test.ts
+++ test/suites/get-secure-input-processes.test.ts
@@ -1,7 +1,6 @@
-import { test } from 'vitest';
-// import { getSecureInputProcesses } from '~/index.js';
+import { test, expect } from 'vitest';
+import { getSecureInputProcesses } from '~/index.js';
 
 test('get secure input processes', () => {
-	// TODO: https://github.com/vitest-dev/vitest/issues/1218
-	// expect(() => getSecureInputProcesses()).not.toThrow();
+	expect(() => getSecureInputProcesses()).not.toThrow();
 });
